59021009a1921c5f731cbf74d008aca484f449a1
sumeet.rana <sumeetsinh28@gmail.com>
2023-01-03 10:29:18 +0530

    backend: initial server setup

---
+++ server/server.ts
@@ -0,0 +1,13 @@
+import cors from 'cors';
+import dotenv from 'dotenv';
+import express from 'express';
+
+const app = express();
+dotenv.config();
+
+app.use(cors());
+
+const PORT = process.env.PORT;
+app.listen(PORT, () => {
+  console.log(`Server running on ${PORT}`);
+})
