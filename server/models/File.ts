961796a9b9b9337ccce2983d450f0e6346131d19
sumeet.rana <sumeetsinh28@gmail.com>
2023-01-03 11:00:22 +0530

    file schema added

---
+++ server/models/File.ts
@@ -0,0 +1,39 @@
+import mongoose, { Document } from "mongoose";
+
+const fileSchema = new mongoose.Schema({
+  filename: {
+    type: String,
+    required: true
+  },
+  secure_url: {
+    type: String,
+    required: true
+  },
+  format: {
+    type: String,
+    required: true
+  },
+  sizeInBytes: {
+    type: String,
+    required: true
+  },
+  sender: {
+    type: String
+  },
+  receiver: {
+    type: String
+  }
+}, {
+  timestamps: true
+})
+
+interface IFile extends Document {
+  filename: string,
+  secure_url: string,
+  sizeInBytes: string,
+  format: string,
+  sender?: string,
+  receiver?: string
+}
+
+export default mongoose.model<IFile>("File", fileSchema);
