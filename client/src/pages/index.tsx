ffa487ee1456623c43bc422d3d14cc6042bc965e
sumeet.rana <sumeetsinh28@gmail.com>
2023-01-03 12:22:01 +0530

    home page basic styling

--- client/src/pages/index.tsx
+++ client/src/pages/index.tsx
@@ -1,9 +1,10 @@
-import Delete from "@components/Delete";
-
 export default function Home() {
   return (
-    <div className="h-screen bg-gray-800">
-      <Delete />
+    <div className="flex flex-col items-center justify-center">
+      <h1 className="my-4 text-3xl font-medium">Got a file? Share it like FAKE NEWS</h1>
+      <div className="w-96 flex flex-col items-center bg-gray-800 shadow-xl rounded-xl justify-center">
+        dropzone
+      </div>
     </div>
   );
 }
